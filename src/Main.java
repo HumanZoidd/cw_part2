923ea97b1627ec30a908700975500f718771d9cc
Sergey Kharitonov <haritonov_s@mail.ru>
2023-04-16 15:31:16 +0400

    Scanner scanner

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println(
-                "Билет 2\nЗадача: напишите программу, которая проверяет, " +
-                        "является ли строка палиндромом.\n");
-        task1();
-        task2();
+                "Билет 2\nЗадача: напишите программу, которая проверяет, является ли строка палиндромом.\n");
+        Scanner scanner = new Scanner(System.in);
+        task1(scanner);
+        task2(scanner);
     }
-        public static void task1() {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Введите число для проверки условия палиндрома ");
+        public static void task1(Scanner scanner) {
+//            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите число для проверки условия палиндрома :  ");
             String examplePleace = scanner.nextLine();
 //            String examplePleace = "1122113";
         int offsetChar = 0;
@@ -27,7 +27,9 @@ public class Main {
         System.out.println("a palindrome");
     }
 
-    public static void task2() {
-
+    public static void task2(Scanner scanner) {
+        System.out.println("Введите число для проверки условия палиндрома :  ");
+        String examplePleace2 = scanner.nextLine();
+        System.out.println("examplePleace2 = " + examplePleace2);
     }
 }
